d92ab9285ec3d8eff856aabb9bd36d1ed6f16216
Cameron Leung <cameron.leung@rogers.com>
2022-10-24 16:51:52 -0400

    fix f-string

--- src/pyspark_ci/add_number.py
+++ src/pyspark_ci/add_number.py
@@ -3,4 +3,4 @@ from pyspark.sql import DataFrame
 
 def add_1(df: DataFrame, col_to_add: str):
     "Add 1 to a column."
-    return df.withColumn("f{col_to_add}_plus_one", F.col(col_to_add) + F.lit(1))
+    return df.withColumn(f"{col_to_add}_plus_one", F.col(col_to_add) + F.lit(1))
